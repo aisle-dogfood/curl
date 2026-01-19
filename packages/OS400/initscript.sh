12a99931c5e0713c83b8b874eb36a370b160b2c7
weareaisle-local-app <Iv23lijiANikYU33q0zF+weareaisle-local-app@users.noreply.github.com>
2026-01-19 23:16:14 +0000

    Apply fix for issue: Unsafe Internal Field Separator (IFS) Manipulation

--- packages/OS400/initscript.sh
+++ packages/OS400/initscript.sh
@@ -144,23 +144,21 @@ canonicalize_path()
         fi
 
         R=
-        IFSSAVE="${IFS}"
-        IFS="/"
-
-        for C in ${P}
-        do      IFS="${IFSSAVE}"
-                case "${C}" in
-                .)      ;;
-                ..)     R="$(expr "${R}" : '^\(.*/\)..*')"
-                        ;;
-                ?*)     R="${R}${C}/"
-                        ;;
-                *)      ;;
-                esac
-        done
-
-        IFS="${IFSSAVE}"
-        echo "/$(expr "${R}" : '^\(.*\)/')"
+        # Use a subshell to isolate IFS changes and prevent global tampering
+        (
+                IFS="/"
+                for C in ${P}
+                do      case "${C}" in
+                        .)      ;;
+                        ..)     R="$(expr "${R}" : '^\(.*/\)..*')"
+                                ;;
+                        ?*)     R="${R}${C}/"
+                                ;;
+                        *)      ;;
+                        esac
+                done
+                echo "/$(expr "${R}" : '^\(.*\)/')"
+        )
 }
 
 
