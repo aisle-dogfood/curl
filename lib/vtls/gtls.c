8354637a511b86532d87df8e9d29f25a02467066
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2026-03-30 17:10:59 +0000

    Apply fix for issue: 0-RTT Early Data Security Bypass in TLS Handshake

--- lib/vtls/gtls.c
+++ lib/vtls/gtls.c
@@ -1071,6 +1071,13 @@ static CURLcode gtls_on_session_reuse(struct Curl_cfilter *cf,
   struct gtls_ssl_backend_data *backend =
     (struct gtls_ssl_backend_data *)connssl->backend;
   CURLcode result = CURLE_OK;
+#ifndef CURL_DISABLE_PROXY
+  const char *pinned_key = Curl_ssl_cf_is_proxy(cf) ?
+    data->set.str[STRING_SSL_PINNEDPUBLICKEY_PROXY] :
+    data->set.str[STRING_SSL_PINNEDPUBLICKEY];
+#else
+  const char *pinned_key = data->set.str[STRING_SSL_PINNEDPUBLICKEY];
+#endif
 
   *do_early_data = FALSE;
   connssl->earlydata_max =
@@ -1081,6 +1088,11 @@ static CURLcode gtls_on_session_reuse(struct Curl_cfilter *cf,
   }
   else if(!Curl_alpn_contains_proto(alpns, scs->alpn)) {
     CURL_TRC_CF(data, cf, "SSL session has different ALPN, no early data");
+  }
+  else if(pinned_key) {
+    /* Do not allow early data when pinned public key is configured,
+     * since the key verification happens after the handshake completes */
+    CURL_TRC_CF(data, cf, "SSL pinned public key configured, no early data");
   }
   else {
     infof(data, "SSL session allows %zu bytes of early data, "
