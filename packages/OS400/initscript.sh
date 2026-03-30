8f10a52ec57a57ad1d08afe93ed5ab6a3b068286
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2026-03-30 13:54:37 +0000

    Apply fix for issue: IFS Tampering in initscript.sh at line 162

--- packages/OS400/initscript.sh
+++ packages/OS400/initscript.sh
@@ -143,24 +143,25 @@ canonicalize_path()
         else    P="$(pwd)/${1}"
         fi
 
-        R=
-        IFSSAVE="${IFS}"
-        IFS="/"
+        # Use a subshell to prevent IFS from affecting the global scope
+        (
+                R=
+                IFS="/"
 
-        for C in ${P}
-        do      IFS="${IFSSAVE}"
-                case "${C}" in
-                .)      ;;
-                ..)     R="$(expr "${R}" : '^\(.*/\)..*')"
-                        ;;
-                ?*)     R="${R}${C}/"
-                        ;;
-                *)      ;;
-                esac
-        done
+                for C in ${P}
+                do
+                        case "${C}" in
+                        .)      ;;
+                        ..)     R="$(expr "${R}" : '^\(.*/\)..*')"
+                                ;;
+                        ?*)     R="${R}${C}/"
+                                ;;
+                        *)      ;;
+                        esac
+                done
 
-        IFS="${IFSSAVE}"
-        echo "/$(expr "${R}" : '^\(.*\)/')"
+                echo "/$(expr "${R}" : '^\(.*\)/')"
+        )
 }
 
 
